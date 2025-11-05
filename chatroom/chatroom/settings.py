5d341f6f9e47903c1f5fa6cfeaf6ac24e5d43406
Ketan Jain <68531877+ketanjain113@users.noreply.github.com>
2025-11-05 19:22:15 +0530

    Update settings.py

--- chatroom/chatroom/settings.py
+++ chatroom/chatroom/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-iy3+5*_3f#0g41m*1djph1b(n(v*@nc^k*2psu1xpckd2m20&x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -137,3 +137,4 @@ connect(host=MONGODB_URI)
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
